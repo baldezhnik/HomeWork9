8e60dcaae8f52b37e321fdf239afb8d7bd7256b2
baldezhnik <drako.dima@gmail.com>
2022-03-26 23:50:32 +0300

    ok, well done

---
+++ src/com/dsd/Main.java
@@ -0,0 +1,61 @@
+package com.dsd;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // String filePath = "src/com/dsd/test1.txt";
+        String outFilePath = "src/Valid.txt"; // Заметил особенность что даже при отсутвии файла, он его создаст.
+        String errorPath = "src/Invalid.txt";
+
+        System.out.println("\tHomeWork");
+        System.out.println("Введите путь к файлу");
+
+        Scanner sc = new Scanner(System.in);
+        String filePath = sc.nextLine();
+
+        try {
+            FileReader fileReader = new FileReader(filePath);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            FileWriter outFileWriter = new FileWriter(outFilePath);
+            BufferedWriter outBufferedWriter = new BufferedWriter(outFileWriter);
+
+            FileWriter outError = new FileWriter(errorPath);
+            BufferedWriter outBufferedError = new BufferedWriter(outError);
+
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                if (checkDocNum(line).equals("")) {
+                    outBufferedWriter.write(line);
+                    outBufferedWriter.flush();
+                    outBufferedWriter.append('\n');
+                } else {
+                    outBufferedError.write(line + ":" + checkDocNum(line));
+                    outBufferedError.flush();
+                    outBufferedError.append('\n');
+                }
+                line = bufferedReader.readLine();
+            }
+        } catch (FileNotFoundException ex1) {
+            System.out.println("Файл не найден");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+
+            System.out.println("\n\tMade by Dmitry Drako (─‿‿─)");
+        }
+
+    }
+
+    static String checkDocNum(String docNum) {
+        String res = (docNum.length() != 15) ? " Не соответствует количество символов." : "";
+        return res + ((docNum.startsWith("docnum") || docNum.startsWith("contract")) ?
+                "" : " Отсутсвует docnum или contract в начале.");
+    }
+}
+
+
